e038cdff8fad78a2600dc2a305d1c59eab136aa5
Kasim <kasimkkhan06@gmail.com>
2025-02-13 13:33:55 +0530

    added disaster images view

--- floodprediction/urls.py
+++ floodprediction/urls.py
@@ -3,4 +3,5 @@ from .views import DisasterSummaryAPIView
 
 urlpatterns = [
     path('api/disaster-summary/', DisasterSummaryAPIView.as_view(), name='disaster_summary'),
+    path('api/disaster-images/' , DisasterImagesAPIView.as_view() , name = 'disaster_images')
 ]
--- floodprediction/views.py
+++ floodprediction/views.py
@@ -3,6 +3,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from .generateSummary import generate_disaster_summary
+from .scrape_images import get_disaster_images
 
 class DisasterSummaryAPIView(APIView):
     def post(self, request, format=None):
@@ -32,6 +33,35 @@ class DisasterSummaryAPIView(APIView):
                 total_deaths=data['total_deaths'],
                 total_injured=data['total_injured'],
                 total_affected=data['total_affected']
+            )
+            return Response({"summary": summary}, status=status.HTTP_200_OK)
+        except Exception as e:
+            return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
+class DisasterImagesAPIView(APIView):
+    def post(self, request, format=None):
+        data = request.data
+        required_fields = [
+            'disaster_date',
+            'month_occurred',
+            'disaster_location',
+            'disaster_type',
+            'total_deaths',
+            'total_injured',
+            'total_affected'
+        ]
+        missing = [field for field in required_fields if field not in data]
+        if missing:
+            return Response(
+                {"error": f"Missing fields: {', '.join(missing)}"},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        try:
+            query = data['disaster_type'] + "in" +  data['disaster_location'] + "in the year" + data['disaster_date']
+            summary = get_disaster_images(
+                query= query , num_images= 10
             )
             return Response({"summary": summary}, status=status.HTTP_200_OK)
         except Exception as e:
