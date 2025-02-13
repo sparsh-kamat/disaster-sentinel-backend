346dad29c6da9db5465298418d1bea4bbc53b4a8
Kasim <kasimkkhan06@gmail.com>
2025-02-13 13:21:37 +0530

    added scraping images

---
+++ floodprediction/scrape_images.py
@@ -0,0 +1,23 @@
+from serpapi import GoogleSearch
+
+def get_disaster_images(query, num_images=10):
+
+    """
+        query format : disaster_type + "in" + disaster_location + "int the year" + "disaster year"
+    """
+    """Fetches disaster-related image URLs using SerpAPI."""
+    
+    api_key = "2a647f362842e169485ded92539033c0810cf5ab93498c8e8b709927e79c1f3a"  # Replace with your SerpAPI key
+
+    params = {
+        "q": query,
+        "tbm": "isch",  # Image search
+        "num": num_images,  # Number of images
+        "api_key": api_key
+    }
+
+    search = GoogleSearch(params)
+    results = search.get_dict()
+
+    image_urls = [img["original"] for img in results.get("images_results", [])[:num_images]]
+    return image_urls
