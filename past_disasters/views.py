cd02a9ec782414ec16b52cc9377e71b8585a81ab
sparsh-kamat <kamatsparsh@gmail.com>
2025-02-28 15:22:20 +0530

    help me query sparsh

--- past_disasters/views.py
+++ past_disasters/views.py
@@ -6,16 +6,21 @@ from rest_framework import filters
 class PastDisasterListView(generics.ListAPIView):
     queryset = PastDisaster.objects.all()
     serializer_class = PastDisasterSerializer
-    filter_backends = (filters.OrderingFilter,)
+    filter_backends = (filters.OrderingFilter, filters.SearchFilter)
     ordering_fields = '__all__'  # Allow ordering by any field in the model
+    search_fields = ['id']  # Add fields you want to search by
 
     def get_queryset(self):
         queryset = PastDisaster.objects.all()
 
-        # Check if the 'state' parameter is in the request
+        # Filter by state (if provided)
         state = self.request.query_params.get('state', None)
         if state:
-            # Filter disasters by state
             queryset = queryset.filter(state=state)
-        
+
+        # Search by disaster ID (if provided)
+        disaster_id = self.request.query_params.get('id', None)
+        if disaster_id:
+            queryset = queryset.filter(id=disaster_id)
+
         return queryset
